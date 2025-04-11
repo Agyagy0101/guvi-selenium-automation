6291e11572e646369344e5993f791ccead0dbbef
Code_debug <161218383+Agyagy0101@users.noreply.github.com>
2025-04-11 12:48:54 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,9 +1,15 @@
-import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pytest
 
 @pytest.fixture
 def browser():
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument('--ignore-certificate-errors')
+    chrome_options.add_argument('--allow-insecure-localhost')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(options=chrome_options)
     driver.maximize_window()
     yield driver
     driver.quit()

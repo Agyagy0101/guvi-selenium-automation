b8f6fc8ecf812b9f6b75955f6bdb98427c3c0719
Code_debug <161218383+Agyagy0101@users.noreply.github.com>
2025-04-11 12:04:16 +0530

    Update test_tc6_login_valid.py

--- tests/test_tc6_login_valid.py
+++ tests/test_tc6_login_valid.py
@@ -3,5 +3,5 @@ from pages.login_page import LoginPage
 def test_valid_login(browser):
     page = LoginPage(browser)
     page.visit("https://www.guvi.in/sign-in/")
-    page.login("valid_email@example.com", "valid_password")
+    page.login("agustineagy23417@gmail.com", "Sandy@01")
     assert page.is_displayed(*page.PROFILE_ICON)
